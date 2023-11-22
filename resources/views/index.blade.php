3550cd51250f284fb3c1d8f46071bb1a94645b43
JioaF <amuhammadsalman45@gmail.com>
2023-11-22 13:20:29 +0700

    finishing the section 1 of homepage and progressing section 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,4 +7,94 @@
         <div class="hero-image">
         </div>
     </section>
+    <section class="section">
+        <header>
+            <h2>Local News Sources</h2>
+        </header>
+        <div class="articles">
+            <section class="news-source">
+                <header class="news-header">
+                    <div class="news-header-title">
+                        <img src="{{ asset('images/news-image.png') }}" alt="">
+                        <h3>Kompas</h3>
+                    </div>
+                    <div class="news-header-button">
+                        <a href="">More</a>
+                    </div>
+                </header>
+                <article class="news">
+                    <div class="news-image">
+                        <img src="{{ asset('images/news-image.png') }}" alt="">
+                    </div>
+                    <div class="news-detail">
+                        <div class="news-info">
+                            <h4>Article Title</h4>
+                            <h5>Category | dd-mm-yyyy</h5>
+                        </div>
+                        <div class="news-button">
+                            <a href="">Read</a>
+                            <a href="">Read Original</a>
+                        </div>
+                    </div>
+                </article>
+                <article class="news">
+                    <div class="news-image">
+                        <img src="{{ asset('images/news-image.png') }}" alt="">
+                    </div>
+                    <div class="news-detail">
+                        <div class="news-info">
+                            <h4>Article Title</h4>
+                            <h5>Category | dd-mm-yyyy</h5>
+                        </div>
+                        <div class="news-button">
+                            <a href="">Read</a>
+                            <a href="">Read Original</a>
+                        </div>
+                    </div>
+                </article>
+            </section>
+            <section class="news-source">
+                <header class="news-header">
+                    <div class="news-header-title">
+                        <img src="{{ asset('images/news-image.png') }}" alt="">
+                        <h3>Detik</h3>
+                    </div>
+                    <div class="news-header-button">
+                        <a href="">More</a>
+                    </div>
+                </header>
+                <article class="news">
+                    <div class="news-image">
+                        <img src="{{ asset('images/news-image.png') }}" alt="">
+                    </div>
+                    <div class="news-detail">
+                        <div class="news-info">
+                            <h4>Article Title</h4>
+                            <h5>Category | dd-mm-yyyy</h5>
+                        </div>
+                        <div class="news-button">
+                            <a href="">Read</a>
+                            <a href="">Read Original</a>
+                        </div>
+                    </div>
+                </article>
+                <article class="news">
+                    <div class="news-image">
+                        <img src="{{ asset('images/news-image.png') }}" alt="">
+                    </div>
+                    <div class="news-detail">
+                        <div class="news-info">
+                            <h4>Article Title</h4>
+                            <h5>Category | dd-mm-yyyy</h5>
+                        </div>
+                        <div class="news-button">
+                            <a href="">Read</a>
+                            <a href="">Read Original</a>
+                        </div>
+                    </div>
+                    </div>
+                </article>
+            </section>
+        </div>
+    </section>
 </x-layouts.app>
